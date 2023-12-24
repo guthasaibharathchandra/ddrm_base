aa19140a3b081bf0f2b4c7bade4a34c406d791d6
guthasaibharathchandra <guthasaibharath06@gmail.com>
2023-12-24 11:35:03 +0100

    code refactoring

--- lib.py
+++ lib.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-#torch.set_default_dtype(torch.float32)
+
 # The numbering of timesteps starts from 1 to T and for index 0, alpha=1, beta=0
 def forward_diffusion(x, t_curr, t_next, schedule): #only for visualization (works with numpy inputs)
     
@@ -209,5 +209,150 @@ class DDPM():
         return loss
 
 
+#------------------------------------------------------ Lib utils for DDRM ------------------------------------------
+
+def sample_timesteps(T_ddpm, T_short=20):
+
+    #Sample a DDRM subsequence of time steps from DDPM schedule
+
+    #INPUT ASSERTIONS: BEGIN
+    assert isinstance(T_ddpm, int)
+    assert isinstance(T_short, int)
+    assert T_short <= T_ddpm and T_short >= 1
+    #INPUT ASSERTIONS: END
+
+    #ALGORITHM: BEGIN
+    T_short_list = [T_ddpm,]
+    s = T_ddpm // T_short    
+    while len(T_short_list) < T_short:
+            t = T_short_list[0] - s
+            t = t if t > 1 else 1
+            T_short_list = [t,] + T_short_list
+    T_short_list = np.asarray(T_short_list)
+    assert len(T_short_list) == T_short
+    return T_short_list
+    #ALGORITHM: END  
 
 
+class DDRM(DDPM):
+    
+    def __init__(self, schedule, model, weightedloss=True, cuda=True,):
+        
+        super().__init__(schedule, model, weightedloss, cuda)
+        self.sigmasquare = (1.0/(self.alpha+1e-12)) - 1.0   
+    
+    def reverse_diffusion_ddrm(self, dummy_x, y, sigma_y, eta, eta_b, inp_channels, image_size, H=None, T_ddrm=20, cuda=True, onlymean=False): #only for generation
+
+        with torch.no_grad():
+                    
+                #INPUT ASSERTIONS AND SETUP: BEGIN
+                assert len(dummy_x.shape) == 4 # this is a random tensor of (batch, c, h, w)
+                assert len(y.shape) == 2
+                device = 'cuda' if cuda else 'cpu'
+                device = torch.device(device)
+                assert H is not None
+                T = len(self.alpha) - 1
+                T_ddrm_list = sample_timesteps(T, T_short=T_ddrm)
+                #INPUT ASSERTIONS AND SETUP: END
+
+
+                #PREPROCESSING: BEGIN  
+                #----------------------------------------------------SVD based pre-processing BEGIN---------------------------------
+                y = y.to(device)
+                dummy_x = dummy_x.to(device)
+                b, m = y.shape
+                fulldimx = dummy_x.shape[1]*dummy_x.shape[2]*dummy_x.shape[3]
+                n = fulldimx
+                
+                #--------------------------------------------
+                singulars = torch.zeros((n),device=dummy_x.device)
+                _singulars = H.singulars() 
+                singulars[:_singulars.shape[0]] = _singulars 
+                assert singulars.shape[0] == n       
+                k = int(torch.sum(singulars > 0))
+                
+                #--------------------------------------------
+                S_inv_diag = torch.zeros((1,n),device=dummy_x.device)
+                S_inv_diag[:,singulars > 0] = (1.0 / singulars)[singulars > 0]
+                
+                S_diag = torch.zeros((1,n),device=dummy_x.device)
+                S_diag[:,singulars > 0] = singulars[singulars > 0]
+                
+                S_mask = torch.zeros((1,n),device=dummy_x.device)
+                S_mask[:,singulars > 0] = 1.0
+                assert k == int(torch.sum(S_mask))
+
+                S_mask_bool = S_mask > 0
+                S_mask_bool = S_mask_bool.reshape((1,n))
+                
+                #--------------------------------------------
+                U_t_y = H.Ut(y) 
+                _y_bar =  U_t_y * S_inv_diag[:,:U_t_y.shape[-1]]  #/ singulars[:U_t_y.shape[-1]]
+                y_bar = torch.zeros((b,n),device=dummy_x.device)
+                y_bar[:,:_y_bar.shape[1]] = _y_bar
+                assert y_bar.shape == (b, n)
+                
+                #----------------------------------------------------SVD based pre-processing END---------------------------------
+                #PREPROCESSING: END
+                
+
+
+                #ALGORITHM: BEGIN                
+                if onlymean:
+                    x_bar_T = torch.zeros_like(y_bar, dtype=torch.float32) #experimental
+                else:
+                    x_bar_T = np.random.normal(loc=(S_mask*y_bar).cpu().numpy(), scale=torch.sqrt(self.sigmasquare[T]*torch.ones_like(y_bar) - torch.square(sigma_y*S_inv_diag)).cpu().numpy())
+                    x_bar_T = torch.tensor(x_bar_T, dtype=torch.float32, device=y_bar.device)
+                    
+                x_inp_bar = x_bar_T
+                x_inp = H.V(x_inp_bar)
+                
+                T_ddrm_list = T_ddrm_list[::-1]
+                T_ddrm_next_list = np.concatenate([T_ddrm_list[1:],[0]])
+                
+                for t_curr, t_prev in zip(T_ddrm_list, T_ddrm_next_list):
+                    
+                        x_inp = x_inp.reshape(dummy_x.shape)
+                        t_curr_inp = t_curr * torch.ones(x_inp.shape[0], dtype=torch.float32, device=x_inp.device)
+                        eps_t_pred = self.infer(torch.as_tensor(np.sqrt(self.alpha[t_curr])*x_inp,dtype=torch.float32), t_curr_inp)
+                        eps_t_pred = eps_t_pred[:,:3] # model also predicts sigma values so 6 channels in total, for mean we only want first 3 channels
+                        x0_pred = x_inp - np.sqrt(self.sigmasquare[t_curr])*eps_t_pred
+                        x0_pred_bar = H.Vt(x0_pred)
+                        assert x0_pred_bar.shape == (b,n)
+
+                        # Setting up weights vector w for each case
+                        w = torch.zeros((1,n),device=x0_pred_bar.device,dtype=torch.float32)
+                        
+                        # CASE3: when sigma_t >= sigma_y / s_i 
+                        mask_sigma_high = S_mask_bool * (sigma_y * S_inv_diag <= np.sqrt(self.sigmasquare[t_prev]))  # eta_b is the weight
+                        w[mask_sigma_high] = eta_b
+
+                        # CASE2: when sigma_t < sigma_y / s_i
+                        mask_sigma_low = S_mask_bool * (sigma_y * S_inv_diag > np.sqrt(self.sigmasquare[t_prev]))
+                        if sigma_y > 0:
+                            w_ = np.sqrt(self.sigmasquare[t_prev]*(1.0 - eta**2)) * S_diag / sigma_y
+                            w[mask_sigma_low] = w_[mask_sigma_low]
+                        else:
+                            assert torch.sum(mask_sigma_high) == k
+                        
+                        # CASE1: when s_i = 0
+                        mask_singular = ~S_mask_bool
+                        w_ = np.sqrt(self.sigmasquare[t_prev]*(1.0 - eta**2)/self.sigmasquare[t_curr])
+                        w[mask_singular] = w_
+
+                        # Weighted average with vector w according to the case
+                        y_bar[:,mask_singular[0]] = x_inp_bar[:,mask_singular[0]]
+                        mean = (1.0 - w)*x0_pred_bar + w*y_bar
+                        std = torch.ones_like(y_bar)*np.sqrt(eta**2 * self.sigmasquare[t_prev])
+                        std[:,mask_sigma_high[0]] = torch.sqrt((self.sigmasquare[t_prev] - (sigma_y * eta_b * S_inv_diag)**2)[0,mask_sigma_high[0]])
+                        assert mean.shape == y_bar.shape and std.shape == mean.shape
+                        
+                        if not onlymean:
+                            x_inp_bar = np.random.normal(loc=mean.cpu().numpy(), scale=std.cpu().numpy())
+                            x_inp_bar = torch.tensor(x_inp_bar, device=y_bar.device, dtype=torch.float32)
+                        else:
+                            x_inp_bar = mean
+                        x_inp = H.V(x_inp_bar)
+                        
+                return x_inp            
+                #ALGORITHM: END
---
+++ runner.py
@@ -0,0 +1,167 @@
+import torch
+import torchvision
+import argparse
+import random
+import os
+import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+
+from guided_diffusion.script_util import create_model, create_classifier, classifier_defaults, args_to_dict
+from datasets import get_dataset, data_transform, inverse_data_transform
+from torchvision.transforms import ToTensor, Compose, PILToTensor, Normalize, Resize
+from lib_svd import Deblurring, SuperResolution
+from lib import make_schedule, DDRM
+from ckpt_util import download
+from collections import namedtuple
+from PIL import Image
+from torchmetrics.functional.image import peak_signal_noise_ratio, structural_similarity_index_measure
+from torchmetrics.image.kid import KernelInceptionDistance
+
+def dict2namespace(config):
+    namespace = argparse.Namespace()
+    for key, value in config.items():
+        if isinstance(value, dict):
+            new_value = dict2namespace(value)
+        else:
+            new_value = value
+        setattr(namespace, key, new_value)
+    return namespace
+
+
+def load_model_and_initialize_ddrm(config_dict, args_dict):
+    
+    # This loads pretrained DDPM model and initializes DDRM class object for reverse process 
+    config = dict2namespace(config_dict)
+    args = dict2namespace(args_dict)
+
+    if not os.path.exists(args.exp):
+            os.mkdir(args.exp)
+
+    model = create_model(**config_dict['model'])
+    if config_dict['model']['use_fp16']:
+            
+            model.convert_to_fp16()
+            
+            if config_dict['model']['class_cond']:
+                ckpt = os.path.join(args.exp, 'logs/imagenet/%dx%d_diffusion.pt' % (config_dict['model']["image_size"], config_dict['model']["image_size"]))
+                if not os.path.exists(ckpt):
+                    download('https://openaipublic.blob.core.windows.net/diffusion/jul-2021/%dx%d_diffusion_uncond.pt' % (config_dict['model']["image_size"], config_dict['model']["image_size"]), ckpt)
+            else:
+                ckpt = os.path.join(args.exp, "logs/imagenet/256x256_diffusion_uncond.pt")
+                if not os.path.exists(ckpt):
+                    download('https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion_uncond.pt', ckpt)
+                
+            model.load_state_dict(torch.load(ckpt, map_location=torch.device("cuda")))
+            model.to(torch.device("cuda"))
+            model.eval()
+            model = torch.nn.DataParallel(model)
+
+    # diffusion schedule
+    schedule = make_schedule(scheme=config_dict['diffusion']['beta_schedule'], rvar='beta', T=config_dict['diffusion']['num_diffusion_timesteps'], 
+                            start_beta=config_dict['diffusion']['beta_start'], end_beta=config_dict['diffusion']['beta_end'])
+                            
+    # DDRM wrapper
+    ddrm_model = DDRM(schedule=schedule,model=model,weightedloss=False,cuda=True)
+
+    return config, args, ddrm_model, schedule
+
+
+def load_dataset_and_task(config, args):
+
+    dataset, test_dataset = get_dataset(args, config)
+    device = torch.device("cuda")
+    args.subset_start = 0
+    args.subset_end = len(test_dataset)
+    print(f'Dataset has size {len(test_dataset)}')    
+
+    def seed_worker(worker_id):
+        worker_seed = args.seed % 2**32
+        np.random.seed(worker_seed)
+        random.seed(worker_seed)
+
+    g = torch.Generator()
+    g.manual_seed(args.seed)
+    val_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=config.sampling.batch_size,
+        shuffle=True,
+        num_workers=config.data.num_workers,
+        worker_init_fn=seed_worker,
+        generator=g,
+    )
+
+    args.sigma_y = 2 * args.sigma_y #to account for scaling to [-1,1]
+    #sigma_y = args.sigma_y
+
+    if args.task == 'deblur':
+        H = Deblurring(torch.Tensor([1/9] * 9).to(device), config.data.channels, config.data.image_size, device)        
+    elif args.task == 'sr4x':
+        H = SuperResolution(config.data.channels, config.data.image_size, ratio=4, device=device)
+    else:
+        raise NotImplementedError
+
+    return dataset, val_loader, H
+
+
+def run_evaluation(config, args, val_loader, ddrm_model, H):
+
+        device = torch.device("cuda")
+        print(f'Start from {args.subset_start}')
+        idx_init = args.subset_start
+        idx_so_far = args.subset_start
+        avg_psnr = 0.0
+        avg_ssim = 0.0
+        pbar = tqdm.tqdm(val_loader)
+        kid = KernelInceptionDistance(normalize=True, compute_on_cpu=False).cuda()
+        
+        for x_orig, classes in pbar:
+            
+            x_orig = x_orig.to(device)
+            x_orig = data_transform(config, x_orig)
+            y = H.H(x_orig)
+            y = y + args.sigma_y * torch.randn_like(y)
+            
+            ##Begin DDRM
+            x = torch.randn(
+                y.shape[0],
+                config.data.channels,
+                config.data.image_size,
+                config.data.image_size,
+                device=device,
+            )
+            
+            with torch.no_grad():
+                x = ddrm_model.reverse_diffusion_ddrm(x, y, args.sigma_y, args.eta, args.eta_b, config.data.channels, config.data.image_size, H=H, T_ddrm=20, cuda=True, onlymean=False)
+        
+            x = inverse_data_transform(config, x)
+            
+            for j in range(x.size(0)):
+                    
+                    orig = inverse_data_transform(config, x_orig[j])
+                    rxorig = orig.reshape((1,*orig.shape))
+                    rx = x[j].to(device).reshape((1,config.data.channels, config.data.image_size, config.data.image_size))
+                    
+                    ssim = structural_similarity_index_measure(rx, rxorig)
+                    #psnr = peak_signal_noise_ratio(rx, rxorig)
+                    mse = torch.mean((rx - rxorig) ** 2)
+                    psnr = 10 * torch.log10(1 / mse)
+                    kid.update(rx, real=False)
+                    kid.update(rxorig, real=True)
+
+                    avg_psnr += float(psnr)
+                    avg_ssim += float(ssim)
+
+            idx_so_far += y.shape[0]
+            pbar.set_description(f"PSNR = {round(avg_psnr / (idx_so_far - idx_init),2)}, SSIM = {round(avg_ssim / (idx_so_far - idx_init),2)}")
+
+        avg_psnr = avg_psnr / (idx_so_far - idx_init)
+        avg_ssim = avg_ssim / (idx_so_far - idx_init)
+        kid_mean, kid_std  = kid.compute()
+        kid_mean, kid_std = round(float(kid_mean*1e3),2), round(float(kid_std*1e3),2)
+        print(f"Total Average PSNR = {round(avg_psnr,2)}")
+        print(f"Total Average SSIM = {round(avg_ssim,2)}")
+        print(f"KID (experimental) = {kid_mean}+-{kid_std}")
+        print("Number of samples: %d" % (idx_so_far - idx_init))
+
+
